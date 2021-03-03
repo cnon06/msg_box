248c1cc23de148554f3f305925a753bec3bf413a
sinan <sinanalagoz74@gmail.com>
2021-03-03 16:01:32 +0300

    v 0.1

---
+++ app/src/main/java/com/example/msg_box/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.msg_box;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void msg_box(String msg)
+    {
+
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage(msg);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+
+
+        alertDialog.show();
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        msg_box("Sinan");
+
+
+
+    }
+}
